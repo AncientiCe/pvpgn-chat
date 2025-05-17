7e3212a0b62f38f4b0c5ccf82fec8e4b802c3c34
Ionut Riza <ionut.riza@gmail.com>
2025-05-17 15:56:34 +0200

    Fix some delays in delivery

--- src/connect.rs
+++ src/connect.rs
@@ -9,39 +9,43 @@ pub struct Connect {
 
 impl Connect {
     pub(crate) fn new(stream: TcpStream) -> Connect {
+        stream.set_nodelay(true).unwrap();
         Connect {stream}
     }
 
     fn waitfor(&mut self, wait_s: &str) {
         let mut buffer = [0; 1024];
-        let mut s: &str = "";
-        while !s.contains(wait_s) {
+        let mut collected = String::new();
+        while !collected.contains(wait_s) {
             let n = self.stream.read(&mut buffer).unwrap();
-            s = std::str::from_utf8(&buffer[..n]).expect("Found invalid utf-8");
+            let part = std::str::from_utf8(&buffer[..n]).expect("Found invalid utf-8");
+            collected.push_str(part);
         }
     }
-    pub(crate) fn connect(&mut self, username: &str, password: &str) -> i32 {
+
+    pub(crate) fn connect(&mut self, username: &str, password: &str) {
         if username.is_empty() || password.is_empty() {
-            return -1;
+            return
         }
 
         self.stream.write_all(&[3]).unwrap();
-
+        self.stream.flush().unwrap();
         println!("Sending username... {}", username);
         self.waitfor("Username:");
         self.stream.write_all(format!("{}\r\n", username).as_bytes()).unwrap();
-
+        self.stream.flush().unwrap();
         println!("Sending password...");
         self.waitfor("Password:");
         self.stream.write_all(format!("{}\r\n", password).as_bytes()).unwrap();
-
+        self.stream.flush().unwrap();
         self.stream.write_all(format!("{}\r\n", "/join w3").as_bytes()).unwrap();
-        return 0;
+        self.stream.flush().unwrap();
     }
 
     pub fn send(&mut self, msg: String) {
         // println!("Sending: {}", msg);
         self.stream.write_all(format!("{}\r\n", msg).as_bytes()).unwrap();
+        self.stream.flush().unwrap();
     }
 }
 
